74f9da97fdfc7887595ef28ab74eaf8949f1f049
lilthanou <85579409+lilthanou@users.noreply.github.com>
2025-02-03 15:11:13 +0100

    v7

--- script.js
+++ script.js
@@ -22,7 +22,8 @@ const line = ".line",
   eight = "#eight",
   eightNine = "#eight-nine",
   nine = "#nine",
-  nineFooter = "#nine-footer";
+  nineFooter = "#nine-footer",
+  backgroundMusic = document.getElementById("audio-control");
 
 let scrollLine = 0,
   levels = [zero, one, two, three, four, five, six, seven, eight, nine],
@@ -30,24 +31,64 @@ let scrollLine = 0,
   interactiveCircles = document.getElementsByClassName("interactive-circle");
   circlesText = document.getElementsByClassName("circle-text");
   innerCircles = document.getElementsByClassName("inner-circle");
+  isAudioOn = false;
 
-for (let i = 0; i < interactiveCircles.length; i++) {
+document.getElementById("transition-sound").volume = 1;
+
+function toggleAudio() {
+  if (!isAudioOn) {
+    backgroundMusic.style.backgroundImage = "url(lib/sound-on.png)";
+    document.getElementById("audio").play();
+    document.getElementById("audio").volume = 0.3;
+    isAudioOn = true;
+  }
+  else {
+    backgroundMusic.style.backgroundImage = "url(lib/sound-off.png)";
+    document.getElementById("audio").pause();
+    isAudioOn = false;
+  }
+}
+
+interactiveCircles[0].addEventListener("mouseover", () => {
+  circlesText[0].style.visibility = "visible";
+  circlesText[0].style.opacity = "1";
+  circlesText[0].style.transition = "color 0.3 ease-in-out 0.5s";
+  circlesText[0].style.textWrap = "wrap";
+  circlesText[0].style.color = "var(--color-3)";
+  interactiveCircles[0].style.animation = "none";
+  interactiveCircles[0].style.width = 25 + "dvw";
+  interactiveCircles[0].style.height = 15 + "dvh";
+  interactiveCircles[0].style.backgroundColor = "rgba(255, 255, 255, 0.07)";
+  innerCircles[0].style.display = "none";
+})
+interactiveCircles[0].addEventListener("mouseout", () => {
+  circlesText[0].style.visibility = "hidden";
+  circlesText[0].style.textWrap = "nowrap";
+  circlesText[0].style.color = "rgba(255, 255, 255, 0)";
+  interactiveCircles[0].style.animation = "pulse-ring 2s cubic-bezier(0.215, 0.61, 0.355, 1) infinite";
+  interactiveCircles[0].style.width = "10dvh";
+  interactiveCircles[0].style.height = "10dvh";
+  interactiveCircles[0].style.backgroundColor = "var(--color-3)";
+  innerCircles[0].style.display = "inline";
+})
+
+for (let i = 1; i < interactiveCircles.length; i++) {
   interactiveCircles[i].addEventListener("mouseover", () => {
     let heightCircle = 20,
         widthCircle = (circlesText[i].innerHTML.length),
-        maxWidth = 32;
+        maxWidth = 33;
         if (widthCircle > maxWidth) {
           heightCircle = widthCircle / maxWidth * 2;
           widthCircle = maxWidth;
-      }
+        }
     circlesText[i].style.visibility = "visible";
     circlesText[i].style.opacity = "1";
     circlesText[i].style.transition = "color 0.3 ease-in-out 0.5s";
     circlesText[i].style.textWrap = "wrap";
-    circlesText[i].style.color = "rgba(255, 255, 255, 0.65)";
+    circlesText[i].style.color = "var(--color-3)";
     interactiveCircles[i].style.animation = "none";
     interactiveCircles[i].style.width = widthCircle + "dvw";
-    interactiveCircles[i].style.height = heightCircle + "dvh";
+    interactiveCircles[i].style.height = heightCircle + 1 + "dvh";
     interactiveCircles[i].style.backgroundColor = "rgba(255, 255, 255, 0.07)";
     innerCircles[i].style.display = "none";
   })
@@ -58,7 +99,7 @@ for (let i = 0; i < interactiveCircles.length; i++) {
     interactiveCircles[i].style.animation = "pulse-ring 2s cubic-bezier(0.215, 0.61, 0.355, 1) infinite";
     interactiveCircles[i].style.width = "10dvh";
     interactiveCircles[i].style.height = "10dvh";
-    interactiveCircles[i].style.backgroundColor = "rgba(255, 255, 255, 0.7)";
+    interactiveCircles[i].style.backgroundColor = "var(--color-3)";
     innerCircles[i].style.display = "inline";
   })
 }
@@ -101,6 +142,25 @@ for (let i = 1; i < levels.length; i++) {
   });
 }
 
+for (let i = 0; i < transitionsLevels.length+1; i++) {
+  gsap.to(transitionsLevels[i], {
+    scrollTrigger: {
+      trigger: transitionsLevels[i],
+      start: "top bottom",
+      end : "top top",
+      onEnter: () => {
+        if (isAudioOn){
+          document.getElementById("transition-sound").play();
+        }
+      },
+      onEnterBack: () => {
+        if (isAudioOn){
+          document.getElementById("transition-sound").play();
+        }      }
+    },
+  });
+}
+
 gsap.to(transitionsLevels[0], {
   scrollTrigger: {
     trigger: transitionsLevels[0],
@@ -111,7 +171,6 @@ gsap.to(transitionsLevels[0], {
         yPercent: (scrollLine = scrollLine - 21.5),
         duration : 0.5,
       });
-      console.log(scrollLine);
     },
     onEnterBack: () => {
       gsap.to(line, {
@@ -152,27 +211,17 @@ gsap.to(transitionsLevels[9], {
     end : "bottom 50%",
     onEnter: () => {
       gsap.to(line, {
-        yPercent: (scrollLine = scrollLine + 2),
+        yPercent: (scrollLine = scrollLine -10 ),
         duration : 0.5,
       });
-      console.log(scrollLine);
+      document.getElementsByClassName("scroll-animation")[0].style.display = "none";
     },
     onEnterBack: () => {
       gsap.to(line, {
-        yPercent: (scrollLine = scrollLine - 2 ),
+        yPercent: (scrollLine = scrollLine + 10 ),
         duration : 0.5,
       });
+      document.getElementsByClassName("scroll-animation")[0].style.display = "flex";
     },
   },
 });
-
-const body = document.body;
-window.addEventListener('scroll', () => {
-  if (window.scrollY > 0) {
-    body.classList.add('scrolled'); // Ajouter la classe scrolled
-    }
-  else {
-    body.classList.remove('scrolled'); // Retirer la classe scrolled
-  }
-});
-
